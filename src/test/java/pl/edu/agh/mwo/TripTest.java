7bc2515bd32a287dbc1f7bf36cc84faf7acc67d2
piter <piter@192.168.1.219>
2018-04-06 09:26:51 +0200

    TripTest ver_1.0

---
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.mwo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class TripTest {
+
+	@Test
+	public void addPhotoTest() {
+		Trip tr1 = new Trip();
+		Photo photo = new Photo("Nowa fotka");
+		assertEquals(0, tr1.getPhotos.size());
+		tr1.addPhoto(photo);
+		assertEquals(1, tr1.getPhotos.size());
+		
+		
+	}
+
+}
